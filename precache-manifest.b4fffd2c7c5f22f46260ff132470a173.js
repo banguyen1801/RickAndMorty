f6d967f2934adc01d486eaed7c83e29763ce1eac
Ba Nguyen <b.pynguyen@gmail.com>
2020-05-08 23:05:21 -0400

    Updates

---
+++ precache-manifest.b4fffd2c7c5f22f46260ff132470a173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4a87bf4a7bafbde4deaa838424260e",
+    "url": "/RickAndMorty/index.html"
+  },
+  {
+    "revision": "567b18a9761172d71ddb",
+    "url": "/RickAndMorty/static/css/main.f67cca7e.chunk.css"
+  },
+  {
+    "revision": "84399148f2da60fa5624",
+    "url": "/RickAndMorty/static/js/2.cb3397eb.chunk.js"
+  },
+  {
+    "revision": "567b18a9761172d71ddb",
+    "url": "/RickAndMorty/static/js/main.0a838882.chunk.js"
+  },
+  {
+    "revision": "82998a1d527543d82c37",
+    "url": "/RickAndMorty/static/js/runtime~main.40b22cce.js"
+  },
+  {
+    "revision": "771c325a79ac09de4fda056f21913589",
+    "url": "/RickAndMorty/static/media/defaultImg.771c325a.jpg"
+  }
+]);
