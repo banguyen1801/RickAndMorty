bc7b47af0fb00313418fb29db17a79d0c36e7603
Ba Nguyen <b.pynguyen@gmail.com>
2020-05-31 17:35:32 -0400

    initial state not received

--- src/index.js
+++ src/index.js
@@ -2,27 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
 import { StoreProvider } from './Store';
 import './index.css';
-
-
 
 import HomePage from './HomePage';
 import { Router } from '@reach/router';
 import FavPage from './FavePage';
 
 ReactDOM.render(
+  <React.StrictMode>
     <StoreProvider>
       <Router>
-        <App path='/'>
-          <HomePage path='/' />
-          <FavPage path='/faves' /> 
+        <App path="/">
+          <HomePage path="/" />
+          <FavPage path="/faves" />
         </App>
       </Router>
-    </StoreProvider>,
-    document.getElementById('root')
-  );
+    </StoreProvider>
+  </React.StrictMode>,
 
-
-
+  document.getElementById('root')
+);
